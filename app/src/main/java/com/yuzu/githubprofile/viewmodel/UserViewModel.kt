fbf0303d935a2fd53064d4dbca9458aec34ff6ad
YUZUFX\user <yustar.shooter@gmail.com>
2021-02-19 11:47:21 +0700

    Add dummy fragment user add some styles

---
+++ app/src/main/java/com/yuzu/githubprofile/viewmodel/UserViewModel.kt
@@ -0,0 +1,11 @@
+package com.yuzu.githubprofile.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+/**
+ * Created by Yustar Pramudana on 18/02/2021
+ */
+
+class UserViewModel(app: Application): AndroidViewModel(app) {
+}
